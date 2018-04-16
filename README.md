d971fee4bde270763200b920f2e916e615538733
mmmaxou <mmmaxou@yahoo.fr>
2018-04-16 21:35:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Presentation Neural network
+Made with bespoke
---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+let bespoke = require('bespoke')
+let classes = require('bespoke-classes')
+let nav = require('bespoke-nav')
+let scale = require('bespoke-scale')
+let bullets = require('bespoke-bullets')
+let hash = require('bespoke-hash')
+let prism = require('bespoke-prism')
+let multimedia = require('bespoke-multimedia')
+let extern = require('bespoke-extern')
+let nebula = require('bespoke-theme-nebula')
+
+// Bespoke.js
+bespoke.from({ parent: 'article.deck', slides: 'section' }, [
+  classes(),
+  nav(),
+  scale(),
+  bullets('.build, .build-items > *:not(.build-items)'),
+  hash(),
+  prism(),
+  multimedia(),
+  nebula(),
+  extern(bespoke)
+])
